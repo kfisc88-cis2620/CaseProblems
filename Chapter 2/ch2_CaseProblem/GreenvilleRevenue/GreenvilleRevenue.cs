274596a84e0a8f430c60d656354b44c103eae2a2
arkais <krfisc@outlook.com>
2020-01-25 01:14:43 -0600

    Added line to display the difference between the years' revenue totals. Added comments.

--- Chapter 2/ch2_CaseProblem/GreenvilleRevenue/GreenvilleRevenue.cs
+++ Chapter 2/ch2_CaseProblem/GreenvilleRevenue/GreenvilleRevenue.cs
@@ -3,7 +3,8 @@
 // Written on: 01/24/20
 // Description: 
 //      Chapter 2 - Case Problem 1
-//      
+//      Prompts user for the number of contestants, then displays the input, calculates the revenue for each
+//      year and displays the total and difference between them.
 // Encountered Issues: None;
 
 using System;
@@ -19,25 +20,31 @@ namespace CaseProblem1
             int entrantsThisYear, 
                 entrantsLastYear;
 
-            double revenueLastYear,
+            double revenueLastYear, 
                 revenueThisYear;
 
             string numContestants;
 
+            // Gets number of contestants from user, converts string to integer, then displays the input.
             Console.WriteLine("Please enter the number of contestants from last year: ");
             numContestants = Console.ReadLine();
             entrantsLastYear = Convert.ToInt32(numContestants);
-            
+            Console.WriteLine("The number of contestants entered this year is {0}.\n", entrantsLastYear);
+
+            // Gets number of contestants from user, converts string to integer, then displays the input.
             Console.WriteLine("Please enter the number of contestants this year: ");
             numContestants = Console.ReadLine();
             entrantsThisYear = Convert.ToInt32(numContestants);
+            Console.WriteLine("The number of contestants entered this year is {0}.\n", entrantsThisYear);
 
+            // Caluclates the revenue for each year
             revenueThisYear = FEE * entrantsThisYear;
             revenueLastYear = FEE * entrantsLastYear;
 
-            Console.WriteLine("Last years revenue was {0}.", revenueLastYear);
-            Console.WriteLine("This years revenue was {0}.", revenueThisYear);
-            
+            // Displays the revenue for each year and calculates the difference between them.
+            Console.WriteLine("This years revenue was {0}.", revenueThisYear.ToString("C"));
+            Console.WriteLine("Last years revenue was {0}.", revenueLastYear.ToString("C"));
+            Console.WriteLine("The difference between this year and last year's revenue was {0}.", (revenueThisYear - revenueLastYear).ToString("C"));
         }
     }
 }
