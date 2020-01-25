36865350760d69907a25e389ec4e9c99f617fe8a
arkais <krfisc@outlook.com>
2020-01-24 20:49:40 -0600

    initial commit for chapter 2 case problem

---
+++ Chapter 2/ch2_CaseProblem/GreenvilleRevenue/GreenvilleRevenue.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CaseProblem1
+{
+    class GreenvilleRevenue
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
