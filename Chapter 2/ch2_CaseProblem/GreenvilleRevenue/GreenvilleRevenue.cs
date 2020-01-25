c997a606f7a5dada5714f4f404798f588da30912
arkais <krfisc@outlook.com>
2020-01-24 20:52:26 -0600

    Added comments to GreenvilleRevenue.cs

--- Chapter 2/ch2_CaseProblem/GreenvilleRevenue/GreenvilleRevenue.cs
+++ Chapter 2/ch2_CaseProblem/GreenvilleRevenue/GreenvilleRevenue.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿// Filename: GreenvilleMotto2.cs
+// Written by: Kelley Fischer & Ian Lundquist
+// Written on: 01/24/20
+// Description: 
+//      Chapter 2 - Case Problem 1
+//      
+// Encountered Issues: None;
+
+using System;
 
 namespace CaseProblem1
 {
