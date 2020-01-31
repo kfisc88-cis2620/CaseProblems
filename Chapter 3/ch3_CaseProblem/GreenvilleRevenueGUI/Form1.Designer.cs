a1d5818159e0da16fe57ffec60cf52ef0da21c99
ianlundquist <60051155+ianlundquist@users.noreply.github.com>
2020-01-31 17:39:37 -0600

    Added the text box for last year

--- Chapter 3/ch3_CaseProblem/GreenvilleRevenueGUI/Form1.Designer.cs
+++ Chapter 3/ch3_CaseProblem/GreenvilleRevenueGUI/Form1.Designer.cs
@@ -29,12 +29,39 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.txtLastYear = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // txtLastYear
+            // 
+            this.txtLastYear.Location = new System.Drawing.Point(172, 76);
+            this.txtLastYear.Name = "txtLastYear";
+            this.txtLastYear.Size = new System.Drawing.Size(100, 20);
+            this.txtLastYear.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtLastYear);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtLastYear;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     }
 }
 
