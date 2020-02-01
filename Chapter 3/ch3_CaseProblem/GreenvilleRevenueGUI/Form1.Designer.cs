3a3412d539f386b68fa8ee71f9ad73f45e8bbbbf
arkais <krfisc@outlook.com>
2020-02-01 15:42:59 -0600

    Added labels, button, and tacky colors

--- Chapter 3/ch3_CaseProblem/GreenvilleRevenueGUI/Form1.Designer.cs
+++ Chapter 3/ch3_CaseProblem/GreenvilleRevenueGUI/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GreenvilleRevenueGUI
 {
-    partial class Form1
+    partial class GreenvilleRevenueGUI
     {
         /// <summary>
         /// Required designer variable.
@@ -32,13 +32,21 @@
             this.txtLastYear = new System.Windows.Forms.TextBox();
             this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.txtThisYear = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.previousRevenueLabel = new System.Windows.Forms.Label();
+            this.currentRevenueLabel = new System.Windows.Forms.Label();
+            this.comparisonLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtLastYear
             // 
-            this.txtLastYear.Location = new System.Drawing.Point(172, 76);
+            this.txtLastYear.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastYear.Location = new System.Drawing.Point(240, 67);
             this.txtLastYear.Name = "txtLastYear";
-            this.txtLastYear.Size = new System.Drawing.Size(100, 20);
+            this.txtLastYear.Size = new System.Drawing.Size(30, 23);
             this.txtLastYear.TabIndex = 0;
             // 
             // contextMenuStrip1
@@ -48,20 +56,96 @@
             // 
             // txtThisYear
             // 
-            this.txtThisYear.Location = new System.Drawing.Point(172, 138);
+            this.txtThisYear.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtThisYear.Location = new System.Drawing.Point(240, 102);
             this.txtThisYear.Name = "txtThisYear";
-            this.txtThisYear.Size = new System.Drawing.Size(100, 20);
+            this.txtThisYear.Size = new System.Drawing.Size(30, 23);
             this.txtThisYear.TabIndex = 2;
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of contestants last year:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of contestants this year:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.calculateButton.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(43, 153);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(72, 33);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            // 
+            // previousRevenueLabel
+            // 
+            this.previousRevenueLabel.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previousRevenueLabel.Location = new System.Drawing.Point(40, 230);
+            this.previousRevenueLabel.Name = "previousRevenueLabel";
+            this.previousRevenueLabel.Size = new System.Drawing.Size(340, 16);
+            this.previousRevenueLabel.TabIndex = 6;
+            // 
+            // currentRevenueLabel
+            // 
+            this.currentRevenueLabel.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentRevenueLabel.Location = new System.Drawing.Point(40, 280);
+            this.currentRevenueLabel.Name = "currentRevenueLabel";
+            this.currentRevenueLabel.Size = new System.Drawing.Size(340, 16);
+            this.currentRevenueLabel.TabIndex = 7;
+            // 
+            // comparisonLabel
+            // 
+            this.comparisonLabel.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comparisonLabel.Location = new System.Drawing.Point(40, 330);
+            this.comparisonLabel.Name = "comparisonLabel";
+            this.comparisonLabel.Size = new System.Drawing.Size(340, 16);
+            this.comparisonLabel.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ContextMenuStrip = this.contextMenuStrip1;
+            this.label6.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(103, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(254, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Greenville Revenue Calculator";
+            // 
+            // GreenvilleRevenueGUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(444, 391);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comparisonLabel);
+            this.Controls.Add(this.currentRevenueLabel);
+            this.Controls.Add(this.previousRevenueLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.txtThisYear);
             this.Controls.Add(this.txtLastYear);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "GreenvilleRevenueGUI";
+            this.Text = "Greenville Revenue Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -72,6 +156,13 @@
         private System.Windows.Forms.TextBox txtLastYear;
         private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
         private System.Windows.Forms.TextBox txtThisYear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label previousRevenueLabel;
+        private System.Windows.Forms.Label currentRevenueLabel;
+        private System.Windows.Forms.Label comparisonLabel;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Chapter 3/ch3_CaseProblem/GreenvilleRevenueGUI/Form1.cs
+++ Chapter 3/ch3_CaseProblem/GreenvilleRevenueGUI/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace GreenvilleRevenueGUI
 {
-    public partial class Form1 : Form
+    public partial class GreenvilleRevenueGUI : Form
     {
-        public Form1()
+        public GreenvilleRevenueGUI()
         {
             InitializeComponent();
         }
