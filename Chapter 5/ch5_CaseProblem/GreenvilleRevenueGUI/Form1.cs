0f9ccafd2409ca621f794c9f02cac247adb5bf74
Kelley Fischer <krfisc@outlook.com>
2020-02-20 12:59:51 -0600

    Copied ch4 case problem and renamed for ch5

---
+++ Chapter 5/ch5_CaseProblem/GreenvilleRevenueGUI/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenvilleRevenueGUI
+{
+    public partial class GreenvilleRevenueGUI : Form
+    {
+        public GreenvilleRevenueGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            const double FEE = 25;
+
+            double lastYear = Convert.ToDouble(txtLastYear.Text);
+            double thisYear = Convert.ToDouble(txtThisYear.Text);
+
+            if (thisYear > lastYear * 2)
+                competitionLabel.Text = "The competition is more than twice as big as last year!";
+            else if (thisYear > lastYear && thisYear <= lastYear * 2)
+                competitionLabel.Text = "The competition is bigger than ever!";
+            else
+                competitionLabel.Text = "A tighter race this year! Come out and cast your vote!";
+
+            previousRevenueLabel.Text = String.Format("The previous year's revenue was {0}", (lastYear * FEE).ToString("C"));
+            currentRevenueLabel.Text = String.Format("The current year's revenue is {0}", (thisYear * FEE).ToString("C"));
+            comparisonLabel.Text = String.Format("The difference between this year and last year's revenue was {0}", ((thisYear - lastYear) * FEE).ToString("C"));
+
+        }
+    }
+}
