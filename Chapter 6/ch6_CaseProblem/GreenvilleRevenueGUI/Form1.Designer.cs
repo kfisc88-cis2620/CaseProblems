f2c25e8c55bf1ad03e302e7dad891e13e6a87193
Kelley Fischer <krfisc@outlook.com>
2020-03-08 16:40:41 -0500

    duplicated chapter 5 for ch6

---
+++ Chapter 6/ch6_CaseProblem/GreenvilleRevenueGUI/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace GreenvilleRevenueGUI
+{
+    partial class GreenvilleRevenueGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtLastYear = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.txtThisYear = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.previousRevenueLabel = new System.Windows.Forms.Label();
+            this.currentRevenueLabel = new System.Windows.Forms.Label();
+            this.comparisonLabel = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.competitionLabel = new System.Windows.Forms.Label();
+            this.errorLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtLastYear
+            // 
+            this.txtLastYear.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastYear.Location = new System.Drawing.Point(240, 67);
+            this.txtLastYear.Name = "txtLastYear";
+            this.txtLastYear.Size = new System.Drawing.Size(30, 23);
+            this.txtLastYear.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // txtThisYear
+            // 
+            this.txtThisYear.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtThisYear.Location = new System.Drawing.Point(240, 102);
+            this.txtThisYear.Name = "txtThisYear";
+            this.txtThisYear.Size = new System.Drawing.Size(30, 23);
+            this.txtThisYear.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(194, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Number of contestants last year:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(194, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Number of contestants this year:";
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.calculateButton.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calculateButton.Location = new System.Drawing.Point(43, 153);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(72, 33);
+            this.calculateButton.TabIndex = 5;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = false;
+            this.calculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // previousRevenueLabel
+            // 
+            this.previousRevenueLabel.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.previousRevenueLabel.Location = new System.Drawing.Point(29, 252);
+            this.previousRevenueLabel.Name = "previousRevenueLabel";
+            this.previousRevenueLabel.Size = new System.Drawing.Size(539, 16);
+            this.previousRevenueLabel.TabIndex = 6;
+            // 
+            // currentRevenueLabel
+            // 
+            this.currentRevenueLabel.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.currentRevenueLabel.Location = new System.Drawing.Point(29, 279);
+            this.currentRevenueLabel.Name = "currentRevenueLabel";
+            this.currentRevenueLabel.Size = new System.Drawing.Size(539, 16);
+            this.currentRevenueLabel.TabIndex = 7;
+            // 
+            // comparisonLabel
+            // 
+            this.comparisonLabel.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comparisonLabel.Location = new System.Drawing.Point(26, 318);
+            this.comparisonLabel.Name = "comparisonLabel";
+            this.comparisonLabel.Size = new System.Drawing.Size(542, 53);
+            this.comparisonLabel.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ContextMenuStrip = this.contextMenuStrip1;
+            this.label6.Font = new System.Drawing.Font("Rockwell", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(103, 20);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(254, 19);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Greenville Revenue Calculator";
+            // 
+            // competitionLabel
+            // 
+            this.competitionLabel.Font = new System.Drawing.Font("Rockwell", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.competitionLabel.Location = new System.Drawing.Point(29, 224);
+            this.competitionLabel.Name = "competitionLabel";
+            this.competitionLabel.Size = new System.Drawing.Size(539, 16);
+            this.competitionLabel.TabIndex = 10;
+            // 
+            // errorLabel
+            // 
+            this.errorLabel.Location = new System.Drawing.Point(285, 85);
+            this.errorLabel.Name = "errorLabel";
+            this.errorLabel.Size = new System.Drawing.Size(269, 20);
+            this.errorLabel.TabIndex = 11;
+            // 
+            // GreenvilleRevenueGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(606, 419);
+            this.Controls.Add(this.errorLabel);
+            this.Controls.Add(this.competitionLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comparisonLabel);
+            this.Controls.Add(this.currentRevenueLabel);
+            this.Controls.Add(this.previousRevenueLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtThisYear);
+            this.Controls.Add(this.txtLastYear);
+            this.Name = "GreenvilleRevenueGUI";
+            this.Text = "Greenville Revenue Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtLastYear;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.TextBox txtThisYear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label previousRevenueLabel;
+        private System.Windows.Forms.Label currentRevenueLabel;
+        private System.Windows.Forms.Label comparisonLabel;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label competitionLabel;
+        private System.Windows.Forms.Label errorLabel;
+    }
+}
+
---
+++ Chapter 6/ch6_CaseProblem/GreenvilleRevenueGUI/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GreenvilleRevenueGUI
+{
+    public partial class GreenvilleRevenueGUI : Form
+    {
+        public GreenvilleRevenueGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            const double FEE = 25;
+
+            double lastYear = Convert.ToDouble(txtLastYear.Text);
+            double thisYear = Convert.ToDouble(txtThisYear.Text);
+
+            if ((lastYear >= 0 && lastYear <= 30) && (thisYear >= 0 && lastYear <= 30))
+            {
+                // Clear error label text
+                errorLabel.Text = "";
+
+                // Compare thisYear no. of contestants to lastYear
+                if (thisYear > lastYear * 2)
+                    competitionLabel.Text = "The competition is more than twice as big as last year!";
+                else if (thisYear > lastYear && thisYear <= lastYear * 2)
+                    competitionLabel.Text = "The competition is bigger than ever!";
+                else
+                    competitionLabel.Text = "A tighter race this year! Come out and cast your vote!";
+
+                previousRevenueLabel.Text = String.Format("The previous year's revenue was {0}", (lastYear * FEE).ToString("C"));
+                currentRevenueLabel.Text = String.Format("The current year's revenue is {0}", (thisYear * FEE).ToString("C"));
+                comparisonLabel.Text = String.Format("The difference between this year and last year's revenue was {0}", ((thisYear - lastYear) * FEE).ToString("C"));
+            } else
+            {
+                // Clear all other label text and display error
+                previousRevenueLabel.Text = "";
+                currentRevenueLabel.Text = "";
+                comparisonLabel.Text = "";
+                competitionLabel.Text = "";
+                errorLabel.Text = "Please enter a valid number from 0 to 30.";
+            }
+        }
+    }
+}
